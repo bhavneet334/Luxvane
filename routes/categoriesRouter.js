be10631b0e0c1e06d423dceaa4441560a4bfc7b7
Bhavneet Kaur <64313291+bhavneet334@users.noreply.github.com>
2025-09-08 12:47:09 -0400

    Add title case formatting and update route for categories

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const Category = require('../models/category-model');
 const isOwnerAuthenticated = require('../middlewares/isOwnerAuthenticated');
@@ -43,6 +44,55 @@ router.post('/create', isOwnerAuthenticated, async function (req, res) {
   }
 });
 
-router.patch('/edit', isOwnerAuthenticated, async function (req, res) {});
+router.patch('/edit/:id', isOwnerAuthenticated, async function (req, res) {
+  try {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid Category ID' });
+    }
+
+    const { name, description, isActive } = req.body;
+
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    if (name !== undefined) {
+      const trimmedName = name.trim();
+      if (!trimmedName) {
+        return res
+          .status(400)
+          .json({ message: 'Category name cannot be empty' });
+      }
+      const duplicate = await Category.findOne({
+        _id: { $ne: id },
+        name: { $regex: new RegExp(`^${trimmedName}$`, 'i') },
+      });
+
+      if (duplicate) {
+        return res.status(409).json({ message: 'Category already exists' });
+      }
+
+      category.name = trimmedName
+        .toLowerCase()
+        .replace(/\b\w/g, (c) => c.toUpperCase());
+
+      category.slug = trimmedName.toLowerCase().replace(/\s+/g, '-');
+    }
+    if (description !== undefined) {
+      category.description = description;
+    }
+    if (isActive !== undefined) {
+      category.isActive = isActive;
+    }
+    await category.save();
+    return res.status(200).json({ message: 'Category updated', category });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 module.exports = router;
